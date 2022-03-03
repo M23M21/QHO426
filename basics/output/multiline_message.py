9dd38c2b37d07cbb3775417a9a2973064e37583c
Marius2021 <>
2022-03-03 19:39:23 +0000

    Y

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,9 @@
+# Display message to the standard output
+print ("System Failure Imminet!")
+print("System reboot has been initiated..")
+
+print("...rebooting sensory system\n")
+print("...rebooting output motors\n")
+print("...rebooting hover engine\n")
+
+print("System online")
